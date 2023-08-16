ba532393de717baf0aa66b99091a10fa3ad58408
tacite <tacite.bahiga.com>
2023-08-16 08:26:16 +0300

    Fix: Setting vite config file for generating service worker file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   build: {
     target: ["edge90", "chrome90", "firefox90", "safari15"],
+
+    rollupOptions: {
+      input: {
+        // the default entry point
+        app: "./index.html",
+
+        // 1️⃣
+        "service-worker": "./service-worker.js",
+      },
+      output: {
+        // 2️⃣
+        entryFileNames: (assetInfo) => {
+          return assetInfo.name === "service-worker"
+            ? "[name].js" // put service worker in root
+            : "assets/js/[name]-[hash].js"; // others in `assets/js/`
+        },
+      },
+    },
   },
 });
